24f99742333b19ce31675f99044ad4d79252dec8
Ruissi Mouhib <mouhibruissi@gmail.com>
2022-11-28 14:55:21 +0100

    gitlab to github

---
+++ mruissi.py
@@ -0,0 +1,33 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+from dotenv import load_dotenv
+import os
+load_dotenv()
+API_KEY=os.getenv("API_KEY")
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'start':'45',
+  'limit':'1',
+  
+
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': API_KEY,
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  json_string= json.dumps(data['data'])
+ 
+  
+  
+  print(json_string)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
